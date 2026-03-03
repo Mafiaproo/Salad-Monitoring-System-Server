12149fbc429809b9f220f342213ba8c345457011
Mafiaproo <gamerduo294@gmail.com>
2026-03-03 21:28:43 +0100

    Initial Commit

---
+++ Salad Monitoring System - Server/Program.cs
@@ -0,0 +1,32 @@
+using Salad_Monitoring_System___Server;
+using System.Text.Json.Nodes;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "Oh, I see that you found the SMS API :) ");
+
+SaladClient client = new SaladClient(Guid.CreateVersion7())
+{
+    ClientName = "Salad-1"
+};
+
+app.MapGet("/api/v1/getConnectedClients", () => client);
+
+app.Run();
---
+++ Salad Monitoring System - Server/SaladClient.cs
@@ -0,0 +1,16 @@
+﻿namespace Salad_Monitoring_System___Server
+{
+    public class SaladClient
+    {
+        public Guid ClientUUID { get; set; }
+        public required string ClientName { get; set; }
+        public Guid InternUUID { get; private set; }
+
+        public SaladClient(Guid iUUID)
+        {
+            ClientUUID = Guid.CreateVersion7();
+            InternUUID = iUUID;
+        }
+
+    }
+}
